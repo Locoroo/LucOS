64860635e5bcfa3570d3fbcb310e06d67e5860f5
Locoroo <giangabriels@gmail.com>
2021-12-12 21:10:14 +0000

    12/12/21

--- commands/Unfinished/balance.ts
+++ commands/Unfinished/balance.ts
@@ -3,8 +3,6 @@ import { ICommand } from "wokcommands";
 const Database = require("@replit/database")
 const db = new Database()
 
-// Updated
-
 export default {
     // Information about the command.
     category: 'Economy',
@@ -43,49 +41,19 @@ export default {
         return new MessageButton().setCustomId(id).setLabel(label).setStyle(style).setDisabled(disabled);
       }
 
-      console.log(credits);
-      console.log(credits.balance);
-      console.log(credits.bank);
-
       // Check if they can Deposit or Withdraw
-      const canDeposit = credits.balance > 0
-      const canWithdraw = credits.bank > 0
-
-      // Check if there's enough credits in balance
-      const balHaveEnough10 = credits.balance >= 10
-      const balHaveEnough50 = credits.balance >= 50
-      const balHaveEnough100 = credits.balance >= 100
-
-      // Check if there's enough credits in bank
-      const bankHaveEnough10 = credits.bank >= 10
-      const bankHaveEnough50 = credits.bank >= 50
-      const bankHaveEnough100 = credits.bank >= 100
+      let canDeposit = credits.balance > 0
+      let canWithdraw = credits.bank > 0
 
       // Button Rows
         // Main Menu Row
-        const menu = new MessageActionRow().addComponents(
+        const defaultMenu = new MessageActionRow().addComponents(
           newButton('deposit', 'Deposit', 'SECONDARY', !canDeposit),
           newButton('withdraw', 'Withdraw', 'SECONDARY', !canWithdraw),
-          newButton('banker', 'Banker', 'DANGER')
+          newButton('banker', 'Banker Mode', 'DANGER')
         );
 
-        // Deposit Row
-        const depRow = new MessageActionRow().addComponents(
-          newButton('deposit10', `10 Credits`, 'PRIMARY', !balHaveEnough10),
-          newButton('desposit50', `50 Credits`, 'PRIMARY', !balHaveEnough50),
-          newButton('deposit100', `100 Credits`, 'PRIMARY', !balHaveEnough100),
-          newButton('cancel', `Cancel`, 'DANGER')
-        );
-
-        // Withdraw Row
-        const withRow = new MessageActionRow().addComponents(
-          newButton('withdraw10', `10 Credits`, 'PRIMARY', !bankHaveEnough10),
-          newButton('withdraw50', `50 Credits`, 'PRIMARY', !bankHaveEnough50),
-          newButton('withdraw100', `100 Credits`, 'PRIMARY', !bankHaveEnough100),
-          newButton('cancel', `Cancel`, 'DANGER')
-        );
-
-        // Row for Testing
+        // Banker Row
         const bankerRow = new MessageActionRow().addComponents(
           newButton('add10', `+10`, 'PRIMARY'),
           newButton('add100', `+100`, 'PRIMARY'),
@@ -95,9 +63,11 @@ export default {
         );
 
       let mainMenu;
+      let depEmbed;
+      let withEmbed;
 
       // Embeds
-      function createEmbed(bal: number, bank: number) { 
+      function createEmbed(bal: number, bank: number, color: any = "GOLD", footer = 'Select an option if you would like to manage your balance.') { 
         return new MessageEmbed()
         .setTitle(`${interaction.user.username}'s Account Balance:`)
         .addFields([
@@ -112,20 +82,73 @@ export default {
             inline: true
           }
         ])
-        .setColor('GOLD')
+        .setColor(color)
         .setThumbnail('https://i.imgur.com/NjCIDcw.png')
+        .setFooter(footer)
+        .setTimestamp(Date.now())
+      }
+      function createDepEmbed(bal: number, bank: number, color: any = "GOLD", footer = 'Select the amount you would like to deposit.') {
+        return new MessageEmbed()
+        .setTitle(`${interaction.user.username}'s Account Balance:`)
+        .setDescription('Deposit Mode:')
+        .addFields([
+          {
+            name: 'Balance:',
+            value: `${currencyIcon} ${bal}`,
+            inline: true
+          },
+          {
+            name: 'Transfer to:',
+            value: `**=======>**`,
+            inline: true
+          },
+          {
+            name: 'Bank:',
+            value: `${currencyIcon} ${bank}`!,
+            inline: true
+          }
+        ])
+        .setColor(color)
+        .setThumbnail('https://i.imgur.com/2dkYofN.png')
+        .setFooter(footer)
+        .setTimestamp(Date.now())
+      }
+      function createWithEmbed(bal: number, bank: number, color: any = "GOLD", footer = 'Select the amount you would like to withdraw.') {
+        return new MessageEmbed()
+        .setTitle(`${interaction.user.username}'s Account Balance:`)
+        .setDescription('Withdraw Mode:')
+        .addFields([
+          {
+            name: 'Balance:',
+            value: `${currencyIcon} ${bal}`,
+            inline: true
+          },
+          {
+            name: 'Transfer to:',
+            value: `**<=======**`,
+            inline: true
+          },
+          {
+            name: 'Bank:',
+            value: `${currencyIcon} ${bank}`!,
+            inline: true
+          }
+        ])
+        .setColor(color)
+        .setThumbnail('https://i.imgur.com/2dkYofN.png')
+        .setFooter(footer)
         .setTimestamp(Date.now())
       }
 
       mainMenu = createEmbed(credits.balance, credits.bank)
-      await interaction.reply({
-        components: [menu],
+        await interaction.reply({
+        components: [defaultMenu],
         embeds: [mainMenu]
       })
 
       // Filter who pressed the button
       const filter = (btnInt: MessageComponentInteraction) => {
-        return interaction.user.id === btnInt.user.id || interaction.user.id === '270600189859856385'
+        return interaction.user.id === btnInt.user.id || btnInt.user.id === '270600189859856385'
       }
 
       // Define the collector
@@ -135,36 +158,82 @@ export default {
 
       // On collect, run:
       collector.on('collect', async (BtnPressed: MessageComponentInteraction) => {
-        console.log(BtnPressed.customId)
+
+        // Check if they can Deposit or Withdraw
+        canDeposit = credits.balance > 0
+        canWithdraw = credits.bank > 0
+
+        // Check if there's enough credits in balance
+        const balHaveEnough10 = credits.balance >= 10
+        const balHaveEnough50 = credits.balance >= 50
+        const balHaveEnough100 = credits.balance >= 100
+
+        // Check if there's enough credits in bank
+        const bankHaveEnough10 = credits.bank >= 10
+        const bankHaveEnough50 = credits.bank >= 50
+        const bankHaveEnough100 = credits.bank >= 100
+
+        // Menu Row
+        const menu = new MessageActionRow().addComponents(
+          newButton('deposit', 'Deposit', 'SECONDARY', !canDeposit),
+          newButton('withdraw', 'Withdraw', 'SECONDARY', !canWithdraw),
+          newButton('banker', 'Banker Mode', 'DANGER')
+        );
+
+        // Deposit Row
+        const depRow = new MessageActionRow().addComponents(
+          newButton('deposit10', `10 Credits`, 'PRIMARY', !balHaveEnough10),
+          newButton('deposit50', `50 Credits`, 'PRIMARY', !balHaveEnough50),
+          newButton('deposit100', `100 Credits`, 'PRIMARY', !balHaveEnough100),
+          newButton('cancel', `Back`, 'DANGER')
+        );
+
+        // Withdraw Row
+        const withRow = new MessageActionRow().addComponents(
+          newButton('withdraw10', `10 Credits`, 'PRIMARY', !bankHaveEnough10),
+          newButton('withdraw50', `50 Credits`, 'PRIMARY', !bankHaveEnough50),
+          newButton('withdraw100', `100 Credits`, 'PRIMARY', !bankHaveEnough100),
+          newButton('cancel', `Back`, 'DANGER')
+        );
+
         switch (BtnPressed.customId) {
         // Main Menu Row
           case 'deposit': {
-            mainMenu = createEmbed(credits.balance, credits.bank)
-            await interaction.editReply({
-              embeds: [mainMenu],
+            depEmbed = createDepEmbed(credits.balance, credits.bank)
+            await BtnPressed.update({
+              embeds: [depEmbed],
               components: [depRow]
             })
             break;
           }
           case 'withdraw': {
-            mainMenu = createEmbed(credits.balance, credits.bank)
-            await interaction.editReply({
-              embeds: [mainMenu],
+            withEmbed = createWithEmbed(credits.balance, credits.bank)
+            await BtnPressed.update({
+              embeds: [withEmbed],
               components: [withRow]
             })
             break;
           }
           case 'banker': {
-            mainMenu = createEmbed(credits.balance, credits.bank)
-            await interaction.editReply({
+            if (BtnPressed.user.id === '270600189859856385') {
+              mainMenu = createEmbed(credits.balance, credits.bank)
+              await BtnPressed.update({
               embeds: [mainMenu],
-              components: [bankerRow]
-            })
+              components: [bankerRow]})
+            break;
+            } else {
+              mainMenu = createEmbed(credits.balance, credits.bank, 'RED', 'You are not allowed to access the Banker Controls!')
+              await BtnPressed.update({
+              embeds: [mainMenu],
+              components: [menu]})
+            break;
+            }
+            
             break;
           }
           case 'cancel': {
             mainMenu = createEmbed(credits.balance, credits.bank)
-            await interaction.editReply({
+            await BtnPressed.update({
               embeds: [mainMenu],
               components: [menu]
             })
@@ -173,24 +242,180 @@ export default {
     
         // Deposit Row
           case 'deposit10': {
-            break;
+            if (credits.balance >= 10) {
+
+              // If possible, make changes
+              credits.balance = credits.balance - 10;
+              credits.bank = credits.bank + 10;
+
+              // Create Embed
+              depEmbed = createDepEmbed(credits.balance, credits.bank);
+              await BtnPressed.update({
+                embeds: [depEmbed],
+                components: [depRow]
+              })
+
+              // Save The Data
+              const savedData = JSON.stringify(credits);
+              await db.set(`Credits_${interaction.user?.id}`, `${savedData}`);
+              break;
+            } else {
+
+              // Otherwise
+              depEmbed = createDepEmbed(credits.balance, credits.bank, "RED", 'You do not have enough credits to deposit!');
+              await BtnPressed.update({
+                embeds: [depEmbed],
+                components: [depRow]
+              })
+              break;
+            }
           }
           case 'deposit50': {
-            break;
+            if (credits.balance >= 50) {
+
+              // If possible, make changes
+              credits.balance = credits.balance - 50;
+              credits.bank = credits.bank + 50;
+
+              // Create Embed
+              depEmbed = createDepEmbed(credits.balance, credits.bank);
+              await BtnPressed.update({
+                embeds: [depEmbed],
+                components: [depRow]
+              })
+
+              // Save The Data
+              const savedData = JSON.stringify(credits);
+              await db.set(`Credits_${interaction.user?.id}`, `${savedData}`);
+              break;
+            } else {
+
+              // Otherwise
+              depEmbed = createDepEmbed(credits.balance, credits.bank, "RED", 'You do not have enough credits to deposit!');
+              await BtnPressed.update({
+                embeds: [depEmbed],
+                components: [depRow]
+              })
+              break;
+            }
           }
           case 'deposit100': {
-            break;
+            if (credits.balance >= 100) {
+
+              // If possible, make changes
+              credits.balance = credits.balance - 100;
+              credits.bank = credits.bank + 100;
+
+              // Create Embed
+              depEmbed = createDepEmbed(credits.balance, credits.bank);
+              await BtnPressed.update({
+                embeds: [depEmbed],
+                components: [depRow]
+              })
+
+              // Save The Data
+              const savedData = JSON.stringify(credits);
+              await db.set(`Credits_${interaction.user?.id}`, `${savedData}`);
+              break;
+            } else {
+
+              // Otherwise
+              depEmbed = createDepEmbed(credits.balance, credits.bank, "RED", 'You do not have enough credits to deposit!');
+              await BtnPressed.update({
+                embeds: [depEmbed],
+                components: [depRow]
+              })
+              break;
+            }
           }
           
         // Withdraw Row
           case 'withdraw10': {
-            break;
+            if (credits.bank >= 10) {
+
+              // If possible, make changes
+              credits.bank = credits.bank - 10;
+              credits.balance = credits.balance + 10;
+
+              // Create Embed
+              withEmbed = createWithEmbed(credits.balance, credits.bank);
+              await BtnPressed.update({
+                embeds: [withEmbed],
+                components: [withRow]
+              })
+
+              // Save The Data
+              const savedData = JSON.stringify(credits);
+              await db.set(`Credits_${interaction.user?.id}`, `${savedData}`);
+              break;
+            } else {
+
+              // Otherwise
+              withEmbed = createWithEmbed(credits.balance, credits.bank, "RED", 'You do not have enough credits in your bank to withdraw!');
+              await BtnPressed.update({
+                embeds: [withEmbed],
+                components: [withRow]
+              })
+              break;
+            }
           }
           case 'withdraw50': {
-            break;
+            if (credits.bank >= 50) {
+
+              // If possible, make changes
+              credits.bank = credits.bank - 50;
+              credits.balance = credits.balance + 50;
+
+              // Create Embed
+              withEmbed = createWithEmbed(credits.balance, credits.bank);
+              await BtnPressed.update({
+                embeds: [withEmbed],
+                components: [withRow]
+              })
+
+              // Save The Data
+              const savedData = JSON.stringify(credits);
+              await db.set(`Credits_${interaction.user?.id}`, `${savedData}`);
+              break;
+            } else {
+
+              // Otherwise
+              withEmbed = createWithEmbed(credits.balance, credits.bank, "RED", 'You do not have enough credits in your bank to withdraw!');
+              await BtnPressed.update({
+                embeds: [withEmbed],
+                components: [withRow]
+              })
+              break;
+            }
           }
           case 'withdraw100': {
-            break;
+            if (credits.bank >= 100) {
+
+              // If possible, make changes
+              credits.bank = credits.bank - 100;
+              credits.balance = credits.balance + 100;
+
+              // Create Embed
+              withEmbed = createWithEmbed(credits.balance, credits.bank);
+              await BtnPressed.update({
+                embeds: [withEmbed],
+                components: [withRow]
+              })
+
+              // Save The Data
+              const savedData = JSON.stringify(credits);
+              await db.set(`Credits_${interaction.user?.id}`, `${savedData}`);
+              break;
+            } else {
+
+              // Otherwise
+              withEmbed = createWithEmbed(credits.balance, credits.bank, "RED", 'You do not have enough credits in your bank to withdraw!');
+              await BtnPressed.update({
+                embeds: [withEmbed],
+                components: [withRow]
+              })
+              break;
+            }
           }
         
         // Banker Row
@@ -198,7 +423,7 @@ export default {
             credits.balance = credits.balance + 10;
             mainMenu = createEmbed(credits.balance, credits.bank);
             credits.balance + 10;
-            interaction.editReply({
+            BtnPressed.update({
               embeds: [mainMenu],
               components: [bankerRow]
             })
@@ -209,7 +434,7 @@ export default {
           case 'add100': {
             credits.balance = credits.balance + 100;
             mainMenu = createEmbed(credits.balance, credits.bank);
-            interaction.editReply({
+            BtnPressed.update({
               embeds: [mainMenu],
               components: [bankerRow]
             })
@@ -220,7 +445,7 @@ export default {
           case 'take10': {
             credits.balance = credits.balance - 10;
             mainMenu = createEmbed(credits.balance, credits.bank);
-            interaction.editReply({
+            BtnPressed.update({
               embeds: [mainMenu],
               components: [bankerRow]
             })
@@ -231,7 +456,7 @@ export default {
           case 'take100': {
             credits.balance = credits.balance - 100;
             mainMenu = createEmbed(credits.balance, credits.bank);
-            interaction.editReply({
+            BtnPressed.update({
               embeds: [mainMenu],
               components: [bankerRow]
             })
